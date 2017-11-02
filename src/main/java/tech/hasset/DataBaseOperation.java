5e63652390da74674d49be910c01126eb7a149bb
Biniam Asnake Kefale <biniamasnake@gmail.com>
2017-11-03 00:34:05 +0100

    Added DataBaseOperation and working!

--- src/main/java/tech/hasset/DataBaseOperation.java
+++ src/main/java/tech/hasset/DataBaseOperation.java
@@ -26,7 +26,7 @@ public class DataBaseOperation {
 
         // Note: JDBC loading and saving can be achieved via either the load/save or jdbc methods
         // Loading data from a JDBC source
-        Dataset<Row> jdbcDF = spark.read()
+       /* Dataset<Row> jdbcDF = spark.read()
                 .format("jdbc")
                 .option("driver","com.mysql.jdbc.Driver")
                 .option("url", "jdbc:mysql:localhost")
@@ -36,24 +36,30 @@ public class DataBaseOperation {
                 .load();
 
         // Displays the content of the DataFrame to stdout
-        jdbcDF.show();
+        jdbcDF.show();*/
 
 
-/*
+
 // Option 1: Build the parameters into a JDBC url to pass into the DataFrame APIs
-        String jdbcUsername = "USER_NAME";
-        String jdbcPassword = "PASSWORD";
-        String jdbcHostname = "HOSTNAME";
-        Integer jdbcPort = 3306;
-        String jdbcDatabase ="DATABASE";
+        String jdbcUsername = "root";
+        String jdbcPassword = "password";
+        String jdbcHostname = "localhost";
+        String jdbcPort = "3306";
+        String jdbcDatabase ="apache-spark-demo";
         String jdbcUrl = "jdbc:mysql://${jdbcHostname}:${jdbcPort}/${jdbcDatabase}?user=${jdbcUsername}&password=${jdbcPassword}";
 
 // Option 2: Create a Properties() object to hold the parameters. You can create the JDBC URL without passing in the user/password parameters directly.
         Properties connectionProperties = new Properties();
-        connectionProperties.put("user", "USER_NAME");
-        connectionProperties.put("password", "PASSWORD");
+        connectionProperties.put("user", "root");
+        connectionProperties.put("password", "password");
 
-        String jdbc_url = "jdbc:mysql://${jdbcHostname}:${jdbcPort}/${jdbcDatabase}";
-        Dataset employees_table = spark.read().jdbc(jdbc_url, "employees", connectionProperties);*/
+        //String jdbc_url = "jdbc:mysql://${jdbcHostname}:${jdbcPort}/${jdbcDatabase}";
+        String jdbc_url = "jdbc:mysql://".concat(jdbcHostname).concat(":").concat(jdbcPort).concat("/").concat(jdbcDatabase).concat("?useSSL=false");
+        System.out.println("constructed jdbc_url: " + jdbc_url);
+
+        Dataset person_table = spark.read().jdbc(jdbc_url, "person", connectionProperties);
+
+        person_table.show();
+        spark.close();
     }
 }
